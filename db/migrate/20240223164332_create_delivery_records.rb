e73ae455983cb279fa549f11328f36a71586b89a
Vita (Shan) Que <vitamiumiu@gmail.com>
2024-04-20 17:04:42 +0800

    minor fix

--- db/migrate/20240223164332_create_delivery_records.rb
+++ db/migrate/20240223164332_create_delivery_records.rb
@@ -1,6 +1,6 @@
 class CreateDeliveryRecords < ActiveRecord::Migration[7.0]
   def change
-    create_table :deliverie_records do |t|
+    create_table :delivery_records do |t|
       t.date :deliver_at
       t.integer :sku_id
       t.integer :sent_count, default: 0
